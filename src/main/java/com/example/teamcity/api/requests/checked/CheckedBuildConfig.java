8692de15350033853cdaae5ecd821ca0689bbdc8
Дмитрий <nic.dima11@mail.ru>
2024-02-03 16:35:51 +0300

    Initial commit

---
+++ src/main/java/com/example/teamcity/api/requests/checked/CheckedBuildConfig.java
@@ -0,0 +1,41 @@
+package com.example.teamcity.api.requests.checked;
+
+import com.example.teamcity.api.models.BuildType;
+import com.example.teamcity.api.requests.CrudInterface;
+import com.example.teamcity.api.requests.Request;
+import com.example.teamcity.api.requests.unchecked.UncheckedBuildConfig;
+import io.restassured.specification.RequestSpecification;
+import org.apache.http.HttpStatus;
+
+public class CheckedBuildConfig extends Request implements CrudInterface {
+    public CheckedBuildConfig(RequestSpecification spec) {
+        super(spec);
+    }
+
+    @Override
+    public BuildType create(Object obj) {
+        return new UncheckedBuildConfig(spec).create(obj)
+                .then().assertThat().statusCode(HttpStatus.SC_OK)
+                .extract().as(BuildType.class);
+    }
+
+    @Override
+    public BuildType get(String name) {
+        return new UncheckedBuildConfig(spec)
+                .get(name)
+                .then().assertThat().statusCode(HttpStatus.SC_OK)
+                .extract().as(BuildType.class);
+    }
+
+    @Override
+    public Object update(Object ob) {
+        return null;
+    }
+
+    @Override
+    public String delete(String id) {
+        return new UncheckedBuildConfig(spec).delete(id)
+                .then().assertThat().statusCode(HttpStatus.SC_NO_CONTENT)
+                .extract().asString();
+    }
+}
---
+++ src/test/java/com/example/teamcity/api/BaseApiTest.java
@@ -0,0 +1,6 @@
+package com.example.teamcity.api;
+
+public class BaseApiTest extends BaseTest{
+
+
+}
